6f4d3e7702479cbd94898d0da2b76125c906ffc1
DavidSuch <davidsuchiapa@gmail.com>
2024-09-07 15:34:08 -0600

    Se agregarón los links dinamicos desde el panel admin, se acabó el proyecto aquí.

--- webpersonal/portfolio/models.py
+++ webpersonal/portfolio/models.py
@@ -5,8 +5,10 @@ class Project(models.Model):
     title = models.CharField(max_length=200, verbose_name="Título")  # A string field for the project title with a max length
     description = models.TextField(verbose_name="Descripción")  # A text field for a detailed project description
     image = models.ImageField(verbose_name="Imagen", upload_to="projects")  # An image field to upload images to a specific folder
+    link = models.URLField(null=True, blank=True, verbose_name="Dirección Web")
     created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creación")  # A datetime field to automatically set the creation date
     updated = models.DateTimeField(auto_now=True, verbose_name="Fecha de Edición")  # A datetime field to automatically update when the record is saved
+    
 
     class Meta:
         verbose_name = "proyecto"
