47957df2731d56c844b949e5b9745b94721f91e8
DavidSuch <davidsuchiapa@gmail.com>
2024-09-07 13:50:54 -0600

    Se cambió el idioma con verbose_name y se agregó un directorio media para agregar imagenes

--- webpersonal/portfolio/models.py
+++ webpersonal/portfolio/models.py
@@ -2,10 +2,17 @@ from django.db import models
 
 # Create your models here.
 class Project(models.Model):
-    title = models.CharField(max_length=200)  # A string field for the project title with a max length
-    description = models.TextField()  # A text field for a detailed project description
-    image = models.ImageField()  # An image field to upload images to a specific folder
-    created = models.DateTimeField(auto_now_add=True)  # A datetime field to automatically set the creation date
-    updated = models.DateTimeField(auto_now=True)  # A datetime field to automatically update when the record is saved
+    title = models.CharField(max_length=200, verbose_name="Título")  # A string field for the project title with a max length
+    description = models.TextField(verbose_name="Descripción")  # A text field for a detailed project description
+    image = models.ImageField(verbose_name="Imagen", upload_to="projects")  # An image field to upload images to a specific folder
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creación")  # A datetime field to automatically set the creation date
+    updated = models.DateTimeField(auto_now=True, verbose_name="Fecha de Edición")  # A datetime field to automatically update when the record is saved
 
+    class Meta:
+        verbose_name = "proyecto"
+        verbose_name_plural = "proyectos"
+        ordering = ["-created"]
+        
     
+    def __str__(self):
+        return self.title
